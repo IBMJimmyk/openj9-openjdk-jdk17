f5c92bb5687f0f3b4762af22e894e44423c67895
jimmyk <jimmyk@ca.ibm.com>
2026-08-10 15:59:10 -0400

    public StringUTF16

--- src/java.base/share/classes/java/lang/StringUTF16.java
+++ src/java.base/share/classes/java/lang/StringUTF16.java
@@ -40,7 +40,7 @@ import jdk.internal.vm.annotation.IntrinsicCandidate;
 import static java.lang.String.UTF16;
 import static java.lang.String.LATIN1;
 
-final class StringUTF16 {
+public final class StringUTF16 {
 
     public static byte[] newBytesFor(int len) {
         if (len < 0) {
